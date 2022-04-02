9e115c300b7017f7a04e384a723b99c2f37df96c
rmward17 <wardrachelm17@gmail.com>
2022-04-02 12:47:11 -0400

    Retirement Info

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -49,3 +49,30 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM dept_emp;
+
+DROP TABLE titles CASCADE;
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
+);
+SELECT * FROM titles;
